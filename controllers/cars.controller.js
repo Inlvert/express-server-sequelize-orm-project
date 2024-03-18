20571e729567aeb78baa14beafa647c54ed821ed
Borys Ushakov <inlvert@gmail.com>
2024-03-19 00:40:26 +0200

    finished basic CRUD

--- controllers/cars.controller.js
+++ controllers/cars.controller.js
@@ -1,3 +1,4 @@
+const e = require("express");
 const { Car } = require("../models");
 
 module.exports.createCar = async (req, res, next) => {
@@ -8,8 +9,70 @@ module.exports.createCar = async (req, res, next) => {
 
     console.log(car);
 
-    res.send(car);
-    
+    res.status(201).send({ data: car });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.getCars = async (req, res, nex) => {
+  try {
+    const car = await Car.findAll();
+
+    console.log(car);
+
+    res.send({ data: car });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.getCar = async (req, res, next) => {
+  try {
+    const {
+      params: { carId },
+    } = req;
+
+    const car = await Car.findByPk(carId);
+
+    console.log(car);
+
+    res.send({ data: car });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.deleteCar = async (req, res, next) => {
+  try {
+    const {
+      params: { carId },
+    } = req;
+
+    const car = await Car.findByPk(carId);
+
+    await car.destroy();
+
+    console.log(car);
+
+    res.status(200).send(car);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.updateCar = async (req, res, next) => {
+  try {
+    const {
+      params: { carId },
+      body,
+    } = req;
+
+    const car = await Car.findByPk(carId);
+
+    const updateCar = await car.update(body);
+
+    res.send({ data: updateCar });
   } catch (error) {
     next(error);
   }
--- routers/carRouter.js
+++ routers/carRouter.js
@@ -1,6 +1,11 @@
 const carRouter = require('express').Router();
 const carController = require('../controllers/cars.controller');
 
-carRouter.post('/', carController.createCar)
+carRouter.post('/', carController.createCar);
+carRouter.get('/', carController.getCars);
+carRouter.get('/:carId', carController.getCar);
+carRouter.delete('/:carId', carController.deleteCar);
+carRouter.put('/:carId', carController.updateCar);
+
 
 module.exports = carRouter;
