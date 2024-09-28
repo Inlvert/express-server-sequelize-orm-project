3c14f31346cd3a631bff285d0cdb908550e29203
Borys Ushakov <inlvert@gmail.com>
2024-09-28 20:58:48 +0300

    add

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = require('./routers');
 const basicErrorsHandler = require('./middlewares/errors/basicErrors');
 const { sequelizeErrorHandler } = require('./middlewares/errors/sequlizeHandler');
+const cors = require('cors')
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
-app.use(sequelizeErrorHandler)
-app.use(basicErrorsHandler)
+app.use(sequelizeErrorHandler);
+app.use(basicErrorsHandler);
 
 module.exports = app;
